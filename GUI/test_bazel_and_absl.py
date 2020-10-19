eee509ae3447a84add21015d0ebb4572f1ff5a62
zhengchaoyou <zhengchaoyou@faw.com.cn>
2020-10-19 23:25:46 +0800

    commit

--- GUI/test_bazel_and_absl.py
+++ GUI/test_bazel_and_absl.py
@@ -4,8 +4,7 @@ from absl import flags
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("name", None, "Your name.")
-flags.DEFINE_integer("num_times", 1,
-                     "Number of times to print greeting.")
+
 
 # Required flag.
 flags.mark_flag_as_required("name")
@@ -13,8 +12,7 @@ flags.mark_flag_as_required("name")
 
 def main(argv):
     del argv  # Unused.
-    for i in range(0, FLAGS.num_times):
-        print('Hello, %s!' % FLAGS.name)
+    print('Hello, %s!' % FLAGS.name)
 
 
 if __name__ == '__main__':
