b831d7bb3b283867ffed164adc40a91bf020df2d
zhengchaoyou <zhengchaoyou@faw.com.cn>
2020-10-20 15:39:20 +0800

    连接mongoDB数据库;保存获取的JSON到文件。

--- GUI/choose_the_scene.py
+++ GUI/choose_the_scene.py
@@ -1,5 +1,6 @@
 import pymongo as mongo
 import yaml
+import json
 
 CONFIG_FILE_NAME = "../config.yaml"
 config = yaml.load(open(CONFIG_FILE_NAME))
@@ -9,18 +10,37 @@ def chooseTheScene(log):
 
     host = config['configuration']['MongoDB']['host']
     port = config['configuration']['MongoDB']['port']
+    db = config['configuration']['MongoDB']['database']
+    collection = config['configuration']['MongoDB']['collection']
 
     client = mongo.MongoClient(host=host, port=port)
     log.info('Configuration ')
 
-    db = client.mydb
-    collection = db.test
+    collection = client.get_database(db).get_collection(collection)
     result = collection.find({},{"_id":1})
+    temp = result.clone()
+
+    print("Here are scenes : ")
     for i in result:
         print(i['_id'])
 
-    scene_number = input("Please choose the scene number : ")
-    print(result)
+    scene_number = int(input("Please choose the scene number : "))
 
+    if scene_number > 0 and scene_number <= temp.count():
+
+        scene_name = temp[scene_number - 1]
+        print("The scene [ "+scene_name['_id']+" ] is choosed" )
+        log.info("The scene [ "+scene_name['_id']+" ] is choosed")
+
+        json_file = collection.find({"_id":scene_name['_id']})
+
+        with open( scene_name['_id']+'.json', 'w') as f:
+            f.write(json.dumps(json_file[0]))
+            f.close()
+
+
+    else:
+        print("Please check the scene number !")
+        log.warning("The scene number is invalid !")
 
     pass
